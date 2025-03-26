e6913cb681e43fa3361498b5ac0ca3d3aedb24e8
Your Nameathirapreman <athirapremk@gmail.com>
2025-03-26 17:47:40 +0530

    seleium first simple pgm

--- src/test/java/selenium1/firstpgm.java
+++ src/test/java/selenium1/firstpgm.java
@@ -1,4 +1,12 @@
 package selenium1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class firstpgm {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://ssdiary.com");        Thread.sleep(5000);
+        driver.quit();
+    }
 }
